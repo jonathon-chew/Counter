2e36981d892a63d50a7a85d3b72815dc14b106ea
Jonathon Chew <>
2023-07-16 00:43:59 +0100

    Increase the number in the button by 1 when clicked

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 //Load internal text upon loading the document
 document.addEventListener("DOMContentLoaded", function() {
+    enterInnerText()
+    counter()
+  });
+
+  function enterInnerText(){
     // Get the reference to the card div
-    var cardDiv = document.getElementById("Card");
+    let cardDiv = document.getElementById("Card");
   
     // Create a text node
-    var textNode = document.createTextNode("This is the added text.");
+    let textNode = document.getElementById.innerHTML = 1;
   
     // Append the text node to the card div
-    cardDiv.appendChild(textNode);
+    cardDiv.innerHTML = textNode;
+  };
+
+function counter(){
+  var counterElement = document.getElementById("Card");
+
+  // Add a click event listener to the element
+  counterElement.addEventListener("click", function() {
+    // Get the current value and convert it to a number
+    var currentValue = parseInt(counterElement.innerHTML);
+
+    // Increment the value by 1
+    var newValue = currentValue + 1;
+
+    // Update the innerHTML of the element with the new value
+    counterElement.innerHTML = newValue;
   });
+}
+
+function newCard(){
+    
+};
